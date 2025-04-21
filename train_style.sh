1d88e94b404f6b18d5932a7f3d8a7cdba7bcb8c9
adarshnagrikar14 <adarshnagrikar1404@gmail.com>
2025-04-22 03:31:41 +0530

    Adjust default configuration and training script parameters for optimized performance and resource allocation

--- train_style.sh
+++ train_style.sh
@@ -6,28 +6,31 @@ export CONFIG="$KAGGLE_PREFIX/default_config.yaml"
 export OUTPUT_DIR="$KAGGLE_PREFIX/models/style_model"
 export TRAIN_DATA="$KAGGLE_PREFIX/data/train/train.jsonl"
 
-CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file $CONFIG train.py \
+CUDA_VISIBLE_DEVICES=0 accelerate launch --config_file $CONFIG train.py \
     --pretrained_model_name_or_path $MODEL_DIR \
-    --cond_size=512 \
-    --noise_size=1024 \
+    --cond_size=256 \
+    --noise_size=512 \
     --subject_column="None" \
     --spatial_column="source" \
     --target_column="target" \
     --caption_column="caption" \
-    --ranks 128 \
-    --network_alphas 128 \
+    --ranks 64 \
+    --network_alphas 64 \
     --output_dir=$OUTPUT_DIR \
     --logging_dir=$LOG_PATH \
-    --mixed_precision="bf16" \
+    --mixed_precision="fp16" \
     --train_data_dir=$TRAIN_DATA \
     --learning_rate=1e-4 \
     --train_batch_size=1 \
     --validation_prompt "K-pop manhwa style" \
-    --num_train_epochs=1000 \
-    --validation_steps=20 \
-    --checkpointing_steps=20 \
+    --num_train_epochs=50 \
+    --validation_steps=100 \
+    --checkpointing_steps=100 \
     --spatial_test_images "$KAGGLE_PREFIX/data/test/test_one.jpeg" \
     --subject_test_images None \
-    --test_h 1024 \
-    --test_w 1024 \
-    --num_validation_images=2
+    --test_h 512 \
+    --test_w 512 \
+    --num_validation_images=2 \
+    --gradient_checkpointing \
+    --cache_latents \
+    --dataloader_num_workers=2
